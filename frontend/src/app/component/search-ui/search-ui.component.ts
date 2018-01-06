f9ac804123d8ed1603ed22a5961d127ba55b3ab0
Daniel <danny1194@live.de>
2018-01-06 12:10:13 +0100

    changed outsourced filter Object

--- frontend/src/app/component/search-ui/search-ui.component.ts
+++ frontend/src/app/component/search-ui/search-ui.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {SearchService} from '../../service/search.service';
 import {Book} from '../../model/Book';
 import {NavigationHeaderComponent} from '../navigation-header/navigation-header.component';
+import {Filter} from '../../model/Filter';
 
 @Component({
   selector: 'search-ui',
@@ -14,29 +15,7 @@ export class SearchUiComponent implements OnInit {
 
   results: Book[];
   tmpResults: Book[];
-  filter = {
-    category: [{name: 'Horror', type: 'category', selected: false},
-      {name: 'Adventure Stories & Action', type: 'category', selected: false},
-      {name: 'Fiction', type: 'category', selected: false},
-      {name: 'Mystery & Thrillers', type: 'category', selected: false},
-      {name: 'Science & Technology', type: 'category', selected: false},
-      {name: 'Computers & Internet', type: 'category', selected: false},
-      {name: 'Politics & History', type: 'category', selected: false},
-      {name: 'Romance', type: 'category', selected: false},
-      {name: 'Sports', type: 'category', selected: false}],
-    delivery: [{name: 'Super Fast', type: 'delivery', selected: false},
-      {name: 'Fast', type: 'delivery', selected: false},
-      {name: 'Normal', type: 'delivery', selected: false}],
-    format: [{name: 'Audiobook', type: 'format', selected: false},
-      {name: 'Paperback', type: 'format', selected: false},
-      {name: 'Hardcover', type: 'format', selected: false}],
-    price: [{name: 0, type: 'price', selected: false},
-      {name: 10, type: 'price', selected: false},
-      {name: 20, type: 'price', selected: false},
-      {name: 30, type: 'price', selected: false},
-      {name: 40, type: 'price', selected: false},
-      {name: 50, type: 'price', selected: false}]
-  };
+  filter = Filter;
 
   searchString: string = ' ';
 
---
+++ frontend/src/app/model/Filter.ts
@@ -0,0 +1,23 @@
+export const Filter = {
+  category: [{name: 'Horror', type: 'category', selected: false},
+    {name: 'Adventure Stories & Action', type: 'category', selected: false},
+    {name: 'Fiction', type: 'category', selected: false},
+    {name: 'Mystery & Thrillers', type: 'category', selected: false},
+    {name: 'Science & Technology', type: 'category', selected: false},
+    {name: 'Computers & Internet', type: 'category', selected: false},
+    {name: 'Politics & History', type: 'category', selected: false},
+    {name: 'Romance', type: 'category', selected: false},
+    {name: 'Sports', type: 'category', selected: false}],
+  delivery: [{name: 'Super Fast', type: 'delivery', selected: false},
+    {name: 'Fast', type: 'delivery', selected: false},
+    {name: 'Normal', type: 'delivery', selected: false}],
+  format: [{name: 'Audiobook', type: 'format', selected: false},
+    {name: 'Paperback', type: 'format', selected: false},
+    {name: 'Hardcover', type: 'format', selected: false}],
+  price: [{name: 0, type: 'price', selected: false},
+    {name: 10, type: 'price', selected: false},
+    {name: 20, type: 'price', selected: false},
+    {name: 30, type: 'price', selected: false},
+    {name: 40, type: 'price', selected: false},
+    {name: 50, type: 'price', selected: false}]
+};
